15b07e010ebb88a73d33dca68bc96468405004f0
hyperdb <hyperdb@gmail.com>
2023-02-04 22:01:35 +0900

    ✨記事一覧を作成するスクリプトを追加

---
+++ list.py
@@ -0,0 +1,36 @@
+import glob
+import re
+import os
+
+
+def main():
+
+    files = glob.glob("./articles/*.md")
+
+    index = []
+    for file in files:
+        f = open(file, 'r', encoding='UTF-8')
+        content = f.read()
+        f.close()
+
+        pattern = r'title: \"［AtCoder］(.*?)\"'
+        result = re.search(pattern, content)
+
+        t = result.group(1)
+        i = os.path.splitext(os.path.basename(file))[0]
+
+        index.append([t, i])
+
+    index.sort()
+
+    f = open('./article_list.md', 'w', encoding='UTF-8')
+    for item in index:
+        title = item[0]
+        id = item[1]
+        line = "- [%s](https://zenn.dev/hyperdb/articles/%s)" % (title, id)
+        f.write(line + "\n")
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
